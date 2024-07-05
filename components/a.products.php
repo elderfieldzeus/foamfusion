5c8743972855e41a21584ba737ec6022b8bae330
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-07-05 20:42:49 +0800

    changed dimensions of dialogs

--- components/a.products.php
+++ components/a.products.php
@@ -21,21 +21,24 @@
                     <img src="../assets/products/<?= $row['VariationImage'] ?>" alt="&nbsp" class="h-[30rem] w-[30rem] object-cover rounded-md">
                 </div>
 
-                <div class="right_dialog w-1/2 flex items-center justify-center">
-                    <form class="wrapper h-[30rem] w-[30rem] flex flex-col justify-between">
-                        <div class="h-20rem">
+                <div class="right_dialog w-1/2">
+                    <form class="wrapper w-[30rem] h-full flex flex-col justify-center">
+                        <div id="inner_form" class="h-4/5 overflow-scroll">
                             <div class="flex items-end gap-2">
                                 <p class="text-4xl"><?= $row['ProductName'] ?></p>
                                 <a href="">
                                     <span class="delete--svg size-6 bg-red-400"></span>
                                 </a>
                             </div>
-                            <p class="text-xl"><input class="focus:outline-none bg-opacity-0 " value="<?=$row['VariationName']?>"></p>
-                            <p class="text-lg mt-4">Mass: <input class="focus:outline-none bg-opacity-0" min="0" value="<?=$row['MassInOZ']?>oz"></p>
-                            <p class="text-lg">Price: Php <input type="numeric" class="focus:outline-none bg-opacity-0" min="0" value="<?=$row['UnitPrice']?>"></p>
-                            <p class="text-lg">In stock: <input type="numeric" class="focus:outline-none bg-opacity-0" min="0" value="<?=$row['InStock']?>"></p>
+                            <p class="text-xl text-gray-400"><input class="focus:outline-none bg-opacity-0 " value="<?=$row['VariationName']?>"></p>
+                            <p class="text-lg mt-4">Mass: <input class="text-gray-400 focus:outline-none bg-opacity-0" min="0" value="<?=$row['MassInOZ']?>oz"></p>
+                            <div class="flex text-lg">
+                                <p>Price:&nbsp</p>
+                                <p class="text-gray-400">Php <input type="numeric" class="focus:outline-none bg-opacity-0" min="0" value="<?=$row['UnitPrice']?>"></p>
+                            </div>
+                            <p class="text-lg">In stock: <input type="numeric" class="text-gray-400 focus:outline-none bg-opacity-0" min="0" value="<?=$row['InStock']?>"></p>
                             <p class="text-xl mt-4">Product Description</p>
-                            <textarea class="focus:outline-none bg-opacity-0 text-lg w-full h-56 resize-none overflow-y-scroll"><?=$row['VariationDescription']?></textarea>
+                            <textarea class="text-gray-400 focus:outline-none bg-opacity-0 text-lg w-full h-56 resize-none overflow-y-scroll"><?=$row['VariationDescription']?></textarea>
                         </div>
 
                         <button type="submit" class="px-6 py-4 bg-yellow-400 rounded-md text-white font-bold">UPDATE</button>
